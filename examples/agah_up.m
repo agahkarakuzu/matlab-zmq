861e8fc5bf08c70b9b2627388798553ef6005e51
agahkarakuzu <agahkarakuzu@gmail.com>
2019-03-04 16:14:19 -0800

    dene

---
+++ examples/agah_up.m
@@ -0,0 +1,12 @@
+address = 'ipc:///tmp/rth2qmrlab';
+
+try
+    zmq.core.disconnect(socket, address);
+catch
+    
+end
+
+context = zmq.core.ctx_new();
+socket = zmq.core.socket(context, 'ZMQ_PUB');
+
+zmq.core.bind(socket, address);
